29cbacfa7ec7b1618ca9a75c5fd20f9a8ae436d6
J C Lawrence <claw@kanga.nu>
2015-08-10 15:53:27 -0700

    Versioneer

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
 #! /usr/bin/env python
 
-try:
-  import pyver
-except ImportError:
-  import pip
-  pip.main (['install', 'pyver'])
-  import pyver # pylint: disable=W0611
-
 from setuptools import setup, find_packages
-
-__version__, __version_info__ = pyver.get_version (pkg = "plantain",
-                                                   public = True)
+import versioneer
 
 setup (
     name = 'plantain',
-    version = __version__,
+    version = versioneer.get_version (),
     description = "Command line tool for managing Mandrill templates.",
     long_description = file ("README.rst").read (),
+    cmdclass = versioneer.get_cmdclass (),
     classifiers = [
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
